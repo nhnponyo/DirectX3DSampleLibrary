701618451c0dda0207fc84d587196578f6d0ccb8
nhnponyo <coponyo@naver.com>
2014-04-01 15:45:55 +0900

    Load Mesh

---
+++ D3DSample.h
@@ -0,0 +1,34 @@
+#pragma once
+#include <d3dx9.h>
+
+#ifdef DIRECTX3DSAMPLELIBRARY_EXPORTS
+#define DIRECTX3DSAMPLELIBRARY_API __declspec(dllexport)
+#else
+#define DIRECTX3DSAMPLELIBRARY_API __declspec(dllimport)
+#endif
+
+class DIRECTX3DSAMPLELIBRARY_API D3DSample
+{
+public:
+	D3DSample();
+	~D3DSample();
+
+private:
+	LPDIRECT3D9			g_pD3D;
+	LPDIRECT3DDEVICE9	g_pd3dDevice;
+
+	LPD3DXMESH          g_pMesh;
+	D3DMATERIAL9*       g_pMeshMaterials;
+	LPDIRECT3DTEXTURE9* g_pMeshTextures; 
+	DWORD               g_dwNumMaterials;
+
+public:
+	HRESULT InitD3D( HWND hWnd );
+	HRESULT InitGeometry();
+	VOID Cleanup();
+	VOID SetupMatrices();
+	VOID Render();
+	static LRESULT WINAPI MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
+	INT CreateSample();
+
+	};
